7329805eff403f948825e251e3f06d7dfa989f3b
dell <nikolaiiislichenko@gmail.com>
2022-11-13 22:50:09 +0200

    create style file for signup

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -10,35 +10,16 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 // import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { useNavigate } from "react-router-dom";
 
 import { registerUser } from '../../fakeDB';
 
 import  { validateSignupForm} from '../../utils';
+import useStyles from './Signup.styles';
 
 //move to utils
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
 
 export default function SignUp() {
   const classes = useStyles();
